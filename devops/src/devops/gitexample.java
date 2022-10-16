1d2a3c2f4f3fd21b2536ee3a714c44a2c6bda000
Sanjay Kumar <skumar@cerashealth.com>
2022-10-16 22:45:09 +0530

    initial commit

---
+++ devops/src/devops/gitexample.java
@@ -0,0 +1,12 @@
+package devops;
+
+public class gitexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("devops example");
+
+	}
+
+}
---
+++ devops/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sanjay kumar
+ *
+ */
+module devops {
+}
